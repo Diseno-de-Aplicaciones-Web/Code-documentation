757143c3df340bab7f9a623fd21a18ad763e4a2d
DanBaDo <byte4life@pm.me>
2022-02-24 13:07:55 +0100

    JavaScript function documentation example

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * Adds two numbers
+ * @param {number} a - Primer número a sumar
+ * @param {number} b - Segundo número a sumar
+ * @returns {number}
+ */
+function sumalo(a,b) {
+    // Lo siguiente no requiere de comentarios
+    return a+b; 
+    // TODO: cosas
+}
